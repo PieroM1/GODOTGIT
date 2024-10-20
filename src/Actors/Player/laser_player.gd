6c8fbb2a2dea31433688f8cb3f08c5e619125b25
Pierix <pierix24@gmail.com>
2024-10-20 01:03:28 -0500

    update:colision laser and asteroid

--- src/Actors/Player/laser_player.gd
+++ src/Actors/Player/laser_player.gd
@@ -1,20 +1,20 @@
-extends CharacterBody2D
+extends Area2D  # Cambiado a Area2D
 
 var direction: Vector2
-
 @export var velocidad:int
 
-
 func _ready():
+	add_to_group("proyectiles")  # Agregar el proyectil al grupo
 	$animacion.play("animation_bullet")
-	$CollisionShape2D.connect("body_entered", Callable(self, "_on_body_entered"))
-	
-func _physics_process(delta):
-	velocity = direction.normalized()*velocidad
-	move_and_slide()
+	$CollisionShape2D.connect("area_entered", Callable(self, "_on_body_entered"))  # Usando Callable correctamente
 
-func _on_body_entered(body):
-	queue_free()
+func _on_body_entered(area):
+	if area.is_in_group("asteroids"):  # Asegúrate de que los asteroides estén en este grupo
+		area.play_explosion_animation()  # Llama al método de explosión del asteroide
+		queue_free()  # Destruye el proyectil
+
+func _process(delta):
+	position += direction.normalized() * velocidad * delta  # Mueve el proyectil
 
 func _on_timer_timeout():
 	queue_free()
