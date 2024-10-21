339e3872031f56339e31f90d6c6992cc05ec00c8
Pierix <pierix24@gmail.com>
2024-10-20 20:35:48 -0500

    add:timer

--- src/environment/Camera2D2.gd
+++ src/environment/Camera2D2.gd
@@ -6,7 +6,6 @@ var suavizado = 0.010
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	jugador = get_node("/root/Level1/Player")
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if jugador:
